9e6c8bf6d989466b797ed4a4dbabf457151c56b2
John3 <johnl.veneri@gmail.com>
2018-08-09 22:15:29 -0500

    update jenkins script

--- packer-templates/scripts/jenkins.sh
+++ packer-templates/scripts/jenkins.sh
@@ -3,12 +3,11 @@
 # JDK and JRE are required for Jenkins
 apt-get install -y openjdk-7-jre openjdk-7-jdk unzip dos2unix
 
-wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
-echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list
 
 apt-get update
-apt-get install -y jenkins
-apt-get upgrade
+apt-get install -y jenkins=1.625.2 daemon
 
 # copy premade configuration files
 # jenkins default config, to set --prefix=jenkins
@@ -25,7 +24,7 @@ cp /tmp/jenkins-config/users/vagrant/config.xml /var/lib/jenkins/users/vagrant
 # example job
 mkdir -p /var/lib/jenkins/jobs
 cd /var/lib/jenkins/jobs
-tar zxvf /tmp/jenkins-config/example-job.tar.gz
+tar zxf /tmp/jenkins-config/example-job.tar.gz
 
 # set permissions or else jenkins can't run jobs
 chown -R jenkins:jenkins /var/lib/jenkins
